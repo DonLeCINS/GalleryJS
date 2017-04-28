93186c6d79feb6996b0138611668b372c3cda246
Don Le <Dle11@mail.csuchico.edu>
2017-04-27 18:46:17 -0700

    lab24

--- gallery.js
+++ gallery.js
@@ -1,6 +1,28 @@
-
-
+var chico = [
+  {description:"Flumes",
+  picture:"http://static.panoramio.com/photos/large/37573219.jpg",
+  from:"panoramio.com"},
+  {description:"Bidwell",
+  picture:"https://upload.wikimedia.org/wikipedia/commons/3/3e/Bidwell_Park_Chico.jpg",
+  from:"wikimedia.org"},
+  {description:"Sierra Nevada brewery",
+  picture:"https://c1.staticflickr.com/3/2745/4070583043_44cbdf99c7_b.jpg",
+  from:"staticflickr.com"},
+  {description:"Bear hole",
+  picture:"http://static.panoramio.com/photos/large/13056243.jpg",
+  from:"panoramio.com"},
+  {description:"Upper Bidwell", picture:"https://farm4.staticflickr.com/3374/3508801408_e7f774a2e0_z.jpg",
+  from:"staticflickr.com"}
+];
+var current;
 function shuffleGallery()
 {
-
+var random;
+do{
+random = Math.floor(Math.random() * chico.length);
+} while(chico[random] == current);
+current = chico[random];
+document.getElementById('picture').src=current.picture;
+document.getElementById('caption').innerHTML=current.description;
+document.getElementById('source').innerHTML="Image courtesy of:"+current.from;
 }
